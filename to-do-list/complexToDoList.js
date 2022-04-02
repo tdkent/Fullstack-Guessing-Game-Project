88453ac9b066bff747eb2b35055bcf8412ae5d78
Tim Kent <timdekent@gmail.com>
2022-04-01 19:35:40 -0700

    strikethrough and delete list items functionality

--- to-do-list/complexToDoList.js
+++ to-do-list/complexToDoList.js
@@ -16,12 +16,20 @@ const todosArr = [];
     //This function will respond to clicks on the checkmark and X buttons once they have been added as lis into the todo list (see renderTodos function below).
     todoList.addEventListener('click', function(event) {
         if (event.target.matches('button')) {
+            //the event is a click. the target is a button inside the todoList variable (which is the ul). the parent of the event.target (the button within the li) is the li.
+            const parent = event.target.parentNode;
+            //dataset is the standard way to read html data attributes in js. parent.dataset.index is the data-index value of the dataset value (the data attribute in html) of the li.
+            const index = parent.dataset.index;
             if (event.target.classList[0] === 'checkButton') {
-                console.log('this is the check button')
+                //if the checkButton button is clicked, the 'complete' key of the corresponding (as determined by the index variable defined above) todo object that has been pushed into the todosArr array, will changed from 'false' to 'true'.
+                todosArr[index].complete = true; 
             }
             if (event.target.classList[0] === 'deleteButton') {
-                console.log('this is the delete button')
+                //if the delete button is clicked, the todosArr is spliced at the index location, and the li will be removed from the page.
+                todosArr.splice(index, 1);
             }
+            //Because this function contains functions that update the state, in order for those changes to be shown to the user, the renderToDos function needs to run each time.
+            renderTodos();
         }
     });
 
@@ -46,11 +54,15 @@ const todosArr = [];
     function renderTodos() {
         let todoListTemplate = '';
         //loop over each object pushed to to the todos array. Insert the value from the the todo.text key (which is the value inputted by the user) into a span, with two buttons next to it, within a list item. Note that a class is added to the button rather than an id, since there will be multiple lis using the same class name.
+        //Data-index attribute is given a value of i and used to store extra data about the li. 
+        //https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
         for (let i = 0; i < todosArr.length; ++i) {
             const todo = todosArr[i];
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+            //The span class uses a conditional (ternary) operator to create an if/else statement. 1. condition (todo.complete -- this is the key/value pair of the todo object), 2. an expression (the class 'checked-off') to execute if the condition evaluates to a truthy value (ie if todo.complete === true), written as ? 'checkedOff', and 3. an expression ('') to execute if falsy (written as : '')
             const todoTemplate = `
-                <li>
-                    <span>${todo.text}</span>
+                <li data-index="${i}">
+                    <span class="${todo.complete ? 'checked-off' : ''}">${todo.text}</span>
                     <button class="checkButton">✅</button>
                     <button class="deleteButton">❌</button>
                 </li>
